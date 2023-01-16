c8fd1bea2cddc6ac10851f24aee5a0145668bc8a
gabrieldonadel <donadeldev@gmail.com>
2023-01-16 09:39:56 -0300

    Add androidStatusBar.translucent true

--- App.tsx
+++ App.tsx
@@ -1,19 +1,41 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeNavigator from "./src/HomeNavigator";
-
-const Stack = createStackNavigator();
+import { ScrollView, View, Text, TextInput, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar style="light" translucent={false} />
+      <ScrollView>
+        {Array.from({ length: 50 }, (_, k) => k).map((i) => (
+          <Text
+            key={i}
+            style={{
+              padding: 10,
+              borderBottomWidth: 1,
+              borderBottomColor: "gray",
+              backgroundColor: "#cccccc",
+            }}
+          >
+            message {i}
+          </Text>
+        ))}
+      </ScrollView>
+      <TextInput
+        placeholder="send a message"
+        style={{ borderWidth: 1, width: "100%" }}
+      />
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
